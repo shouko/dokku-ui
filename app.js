6798153afddc1018398bfe9f16b808ded22c9058
shouko <imshouko@gmail.com>
2016-06-19 04:08:48 +0800

    add login route

--- app.js
+++ app.js
@@ -3,50 +3,72 @@ var express = require('express');
 var serveStatic = require('serve-static');
 var bodyParser = require('body-parser')
 var Sequelize = require('sequelize');
+var cookieSession = require('cookie-session')
 var sequelize = new Sequelize(config.db_url);
-var Models = require('./models')(Sequelize, sequelize);
+var models = require('./models')(Sequelize, sequelize);
 sequelize.sync();
 
 var app = express();
 
 app.use(serveStatic('public'));
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieSession({
+  name: 'session',
+  keys: [config.cookie_key1, config.cookie_key2]
+}));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
   res.render('index', {
-      title: config.title
+    title: config.title
   });
 });
 
-app.get('/app/list', function (req, res) {
-    res.render('app_list', {
-      title: config.title + " - App"
+app.get('/app/list', loggedIn, function (req, res) {
+  res.render('app_list', {
+    title: config.title + " - App"
   });
 });
 
-app.get('/app/info/:name', function(req, res){
+app.get('/app/info/:name', loggedIn, function(req, res){
 	res.render('app_info', {
-        title: config.title + " - App Information"
-    });
+    title: config.title + " - App Information"
+  });
 });
 
-app.get('/app/create', function (req, res) {
+app.get('/app/create', loggedIn, function (req, res) {
   res.render('app_create', {
-      title: config.title + " - Create New App"
+    title: config.title + " - Create New App"
   });
 });
 
-app.get('/database', function (req, res) {
+app.get('/database', loggedIn, function (req, res) {
   res.render('database_list', {
-      title: config.title + " - Database"
+    title: config.title + " - Database"
   });
 });
 
-app.get('/database/create', function(req, res){
+app.get('/database/create', loggedIn, function(req, res){
 	res.render('database_create', {
-        title: config.title + " - Create New Database"
+    title: config.title + " - Create New Database"
+  });
+});
+
+app.post('/login', function(req, res) {
+  if(!req.body.username || !req.body.password) {
+    return res.end();
+  }
+  models.User.findOne({
+    username: req.body.username
+  }).then(function(user) {
+    user.verifyPassword(req.body.password).then(function() {
+      req.session.username = user.username;
+      res.redirect('/app/list');
     });
+  }).error(function(err) {
+    req.session.flash = 'Wrong Password';
+    res.redirect('/login');
+  });
 });
 
 app.get('/hello/:name', function (req, res) {
--- models/user.js
+++ models/user.js
@@ -1,3 +1,6 @@
+var Promise = require('bluebird');
+var bcrypt = require('bcrypt-nodejs');
+
 module.exports = function(Sequelize, sequelize) {
   return sequelize.define('user', {
     username: {
@@ -6,5 +9,23 @@ module.exports = function(Sequelize, sequelize) {
     },
     password: Sequelize.STRING,
     email: Sequelize.STRING
+  }, {
+    hooks: {
+      beforeCreate: function(user, options) {
+        user.password = bcrypt.hashSync(user.password);
+      }
+    },
+    instanceMethods: {
+      verifyPassword: function(password) {
+        return new Promise(function(resolve, reject) {
+          bcrypt.compare(password, this.password, function(err, res) {
+            if(err) {
+              return reject(err);
+            }
+            resolve(res);
+          })
+        });
+      }
+    }
   });
 };
