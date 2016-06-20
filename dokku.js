1b6c85a4c79c6406ac65e70b6c33cb0a05e2935c
shouko <imshouko@gmail.com>
2016-06-20 14:56:54 +0800

    fix ssh command

--- dokku.js
+++ dokku.js
@@ -3,7 +3,7 @@ var child_process = require('child_process');
 
 module.exports = function(args, input) {
   var stdout, stderr;
-  child_process.spawnSync('ssh', ['ssh', '-i', config.identity, 'dokku@' + config.dokku_host].concat(args), {
+  child_process.spawnSync('ssh', ['-i', config.identity, 'dokku@' + config.dokku_host].concat(args), {
     input: input,
     stdout: stdout,
     stderr: stderr
